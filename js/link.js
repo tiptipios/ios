c85f7c5c39d160a8245cc35a2e7aa93be5b8f843
tiptipios <131225223+tiptipios@users.noreply.github.com>
2024-08-29 23:55:45 +0700

    Add files via upload

---
+++ js/link.js
@@ -0,0 +1,20 @@
+function yc() {
+    if (window.ycpd == 1) {
+        window.ycpd = 0;
+        setButtonImage("https://cdn.jsdelivr.net/gh/tiptipios/ios@main/tiptip.jpg");
+    } else {
+        window.ycpd = 1;
+        setButtonImage("https://cdn.jsdelivr.net/gh/tiptipios/ios@main/tiptip.jpg");
+    }
+}
+
+function DC() {
+    location.href = "https://t.me/gamemodios";
+}
+
+function YT() {
+    location.href = "https://t.me/freefiretiptip";
+}
+function BC() {
+    location.href = "https://t.me/Chattiptip";
+}
