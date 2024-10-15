aada1680a5f54a392ae0b880bf80aa2908ae1e46
tiptipios <131225223+tiptipios@users.noreply.github.com>
2024-10-15 21:25:34 +0700

    Update link8.js

--- js/link8.js
+++ js/link8.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: "#app", data() {
         return {
-            password: localStorage.getItem("savedPassword") || "", correctPassword: "TipTipFreeUpdate1/10", isPasswordCorrect: !1,
-            linkToCopy: 'https://3Link.Co/AaN8',
+            password: localStorage.getItem("savedPassword") || "", correctPassword: "TipTipFree10", isPasswordCorrect: !1,
+            linkToCopy: 'https://3Link.Co/bGYwqLm',
             maxAttempts: 99999993,
             currentAttempt: 0,
             ifshow: !0,
